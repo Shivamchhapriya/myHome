ab9eaae7e49e48ec08427cf03918520f8d92d8d6
Shivamchhapriya <shivamchhapriya2003@gmail.com>
2023-11-04 00:43:02 +0530

    myHome

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.js';
+// import 'bootstrap/dist/css/bootstrap';
+
+import Navbar from './component/Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
