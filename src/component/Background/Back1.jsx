edca420fbf77b3a5bacb8df9d0a4cd68269a83b4
kapil260601 <88288424+kapil260601@users.noreply.github.com>
2023-11-04 18:33:51 +0530

    page

---
+++ src/component/Background/Back1.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Back1.css";
+function Back1() {
+  return (<>
+  <div>
+    <div className="row contain mt-5 pt-0 pb-4">
+            <div className='col-sm-8 mt-5 ps-5'>
+                <div className="w-100">
+                    <div className="w-100">
+                    <h1 className='header1 mb-3'>Explore Premier House Plans And Home Designs For</h1>
+                    <h1 className='header2'>Your Dream Residence</h1>
+                    <p className='pt-2'>We are a one-stop solution for all your house design needs, with an experience of more than 9 years in providing<br/> all kinds of architectural and interior services. Whether you are looking for a floor plan. elevation design, structure<br/> plan, working drawings, or other add-on services. we have got you covered.</p>
+                    <p className='pt-2'>Our qualified and experienced team of engineers, architects and designers get together to create home designs<br/> which suit all palettes and preferences. Our aim has always been strong commitment towards customer satisfaction,<br/> and intricate detailing in all our projects.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-sm-4 p-0 '>
+            <img alt='about us image' src='./asset/dream-home.png' width="100%"/>
+            </div>
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default Back1
---
+++ src/component/Background1/Back11.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import "./Back11.css";
+function Back11() {
+  return (<>
+    <div className='container-fluid'>
+       <div className="row">
+            <div className="sm-12">
+                <h3 className='header_3 mt-5'>
+                Your One-Stop Destination For Home Plans And Designs
+                </h3>
+                <div className="create_div">
+                    <div className="inner-create_div"></div>
+                </div>
+            </div>
+       </div>
+       <div className='row pt-5'>
+           <div className="col-sm-3 mb-5">
+              <a href='https://plotkhareedo.com/' target='_blank' >
+              <div className='Outer_div Outer_div-2'>
+               <img className='alignment' src='https://www.makemyhouse.com//assets/themelibv3assets/images/plot-logo.png' />
+                 <div className='Outer_div_icon'>
+                    <img src='	https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/e-2.png?version=210' loading='lazy' />
+                 </div>
+                 <h3 className='mt-3'>Buy & Sell Plot </h3>
+                 <ul className='mt-2 '>
+                   <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' /> Easy registration on Plot Khareedo</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' /> Geo location-based plot search</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' /> Thousands of verified sellers</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' /> 60+ cities available</li>
+                 </ul>
+              </div>
+              </a>
+           </div>
+           <div className="col-sm-3 mb-5">
+              <a href='https://www.masonsandmortar.com/' target='_blank' >
+              <div className='Outer_div Outer_div-3'>
+               <img className='alignment' src='https://www.makemyhouse.com//assets/themelibv3assets/images/masons.png' />
+                 <div className='Outer_div_icon'>
+                    <img src='	https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/e-3.png?version=210' loading='lazy' />
+                 </div>
+                 <h3 className='mt-3'> Turnkey Construction </h3>
+                 <ul className='mt-2 '>
+                   <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />100% Quality Assurance </li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Safe money transaction Model </li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Assured Quality controls </li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Track progress on app</li>
+                 </ul>
+              </div>
+              </a>
+           </div>
+           <div className="col-sm-3 mb-5">
+              <a href='https://yellowcap.in/' target='_blank' >
+              <div className='Outer_div Outer_div-4'>
+               <img className='alignment' src='	https://www.makemyhouse.com//assets/themelibv3assets/images/yellow-cap.png' />
+                 <div className='Outer_div_icon'>
+                    <img src='	https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/e-4.png?version=210' loading='lazy' />
+                 </div>
+                 <h3 className='mt-3'>Contractors & Labors</h3>
+                 <ul className='mt-2 '>
+                   <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />1 Lacs+ verified Contractors</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />List your Project in few clicks</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Easy chat system to connect.</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Hire contractor for free</li>
+                 </ul>
+              </div>
+              </a>
+           </div>
+           <div className="col-sm-3 mb-5">
+              <a href='https://www.makemyhouse.com/site/site_supervision' target='_blank' >
+              <div className='Outer_div Outer_div-5'>
+               {/* <img className='alignment' src='https://www.makemyhouse.com//assets/themelibv3assets/images/plot-logo.png' /> */}
+                 <div className='Outer_div_icon'>
+                    <img src='https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/e-5.png?version=210' loading='lazy' />
+                 </div>
+                 <h3 className='mt-3'>Site Supervision & Quality Audit</h3>
+                 <ul className='mt-2 '>
+                   <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />QA/QC Services</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Home Inspect Services</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Fire Safety Audit</li>
+                    <li><img className='me-2' src='https://www.makemyhouse.com//assets/themelibv3assets/images/check.png' />Structural Safety Audit</li>
+                 </ul>
+              </div>
+              </a>
+           </div>
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default Back11;
