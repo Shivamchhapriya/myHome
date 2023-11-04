3086a77d580b182f819dece1578ebac0cbfbddf7
cod3rjava <cod3rjava@gmail.com>
2023-11-05 00:27:32 +0530

    dash

---
+++ src/component/Carosuael/HearFrom.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useRef } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import  './First.css'
+
+const HearFrom= () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 800 },
+          items: 3,
+          slidesToSlide: 1// optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 768 },
+          items: 4,
+          slidesToSlide: 4 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 767, min: 464 },
+          items: 2,
+          slidesToSlide: 2  // optional, default to 1.
+        }
+      };
+      const sliderImageUrl = [
+        //First image url
+        {
+            city:"Haryana",
+            name:"Piyush",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Madhya Pradesh",
+            name:"Kapil",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Punjab",
+            name:"Shivam",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Uttar Pradesh",
+            name:"Cherry",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Indore",
+            name:"Sachin",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Sanawad",
+            name:"Guru",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+        {
+            city:"Pune",
+            name:"Ayush",
+            content:"lorem ipsum dolor sit amet, consectetur adip non pro id el element i    id. Lorem ipsum dolor sit amet, consectet tempor invidunt ut labore et dolore magna aliqu sapien null null. Lorem ipsum dolor sit amet, consectet   tempor invidunt ut labore et dolore magna aliqu sap"
+        },
+       
+    
+      ];
+
+      const carouselRef = useRef(null);
+      
+      function CustomDot(props) {
+        const { onClick, index, active } = props;
+      
+        return (
+          <button
+            onClick={onClick}
+            style={{
+              width: '10px',
+              height: '10px',
+              borderRadius: '50%',
+              backgroundColor: active ? '#3bad4b' : '#3bad4b85', // Change the active and inactive dot color here
+              margin: '10px 5px 5px 5px',
+              cursor: 'pointer',
+              border: 'none',
+            }}
+          ></button>
+        );
+      }
+  return (
+    <div className="px-3 mt-5"> 
+    <center> 
+        <b style={{fontSize:"25px"}}>Hear From Our Satisfied Homeowners</b>
+        <div className="create_div mb-5">
+                    <div className="inner-create_div"></div>
+                </div>
+    </center>
+    <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        arrows={false}
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        infinite={true}
+        pauseOnHover
+        partialVisible={false}
+        rtl={false}
+        dotListClass="custom-dot-list-style"
+        ref={carouselRef}
+        customDot={<CustomDot />}
+      >
+      {sliderImageUrl.map((imageUrl, index) => {
+        return (
+          <div className="slider p-3 m-2 rounded-3 justify-content-center border border-secondary" key={index} >
+            <div className='row'>
+                <div className='col-2'>
+                <img src={"https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/user.png?version=210"} alt="movie" width={"37px"}className=''/>
+                </div>
+                <div className='col-10'>
+                <h4><b>{imageUrl.name} </b></h4>
+                <span style={{color:"green"}}>({imageUrl.city})</span>
+                </div>
+                <p className='mt-3'>⭐️⭐️⭐️⭐️⭐️</p>
+                <p>{imageUrl.content}</p>
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
+  </div>
+  );
+};
+
+export default HearFrom;
---
+++ src/component/Carosuael/Showcasing.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useRef } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import  './First.css'
+
+const Showcasing= () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 800 },
+          items: 5,
+          slidesToSlide: 1// optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 768 },
+          items: 4,
+          slidesToSlide: 4 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 767, min: 464 },
+          items: 2,
+          slidesToSlide: 2  // optional, default to 1.
+        }
+      };
+      const sliderImageUrl = [
+        //First image url
+        {
+          url:
+            "https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-10.webp?version=210",
+            name:"Tamil Nadu",
+            project:"68"
+        },
+        {
+          url:
+            "https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-13.webp?version=210",
+            name:"Telangan",
+            project:"78"
+        },
+        {
+          url:
+            "https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-16.webp?version=210",
+            name:"Uttrakhand",
+            project:"199"
+        },
+        {
+          url:
+            "https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-2.webp?version=210",
+            name:"Karnataka",
+            project:"120"
+        },
+        {
+          url:
+            "https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-3.webp?version=210",
+            name:"Rajisthan",
+            project:"133"
+        },
+        {
+          url:
+            "	https://www.makemyhouse.com/assets/themelibv2assets/webview/images/cp-15.webp?version=210",
+            name:"Haryana",
+            project:"36"
+        },
+       
+    
+      ];
+
+      const carouselRef = useRef(null);
+      const CustomLeftButton = () => (
+        <button
+          className="custom-button left-button"
+          onClick={() => carouselRef.current.previous()}
+        >
+                  <img src="https://www.makemyhouse.com//assets/themelibv3assets/images/left-errow.png" alt="" />
+
+        </button>
+      );
+    
+      const CustomRightButton = () => (
+        <button
+          className="custom-button right-button"
+          onClick={() => carouselRef.current.next()}
+        >
+         <img src="	https://www.makemyhouse.com//assets/themelibv3assets/images/right-errow.png" alt="" />
+
+        </button>
+      );
+      const CustomButtonGroup = (props) => {
+        return (
+          <div className="custom-button-group">
+          
+              <>
+                <CustomLeftButton {...props} />
+                <CustomRightButton {...props} />
+              </>
+       
+          </div>
+        );
+      };
+  return (
+    <div className="px-3 mt-5"> 
+    <center> 
+        <b style={{fontSize:"25px"}}>Showcasing Our Pan-India Home Design Projects</b>
+        <div className="create_div mb-5">
+                    <div className="inner-create_div"></div>
+                </div>
+    </center>
+    <Carousel
+      responsive={responsive}
+      autoPlay={true}
+      arrows={false}
+      swipeable={true}
+      draggable={true}
+      showDots={false}
+      infinite={true}
+      pauseOnHover
+      partialVisible={false}
+      rtl={false}
+      autoPlaySpeed={2000} 
+      dotListClass="custom-dot-list-style"
+        // customButtonGroup={<CustomButtonGroup />}
+        ref={carouselRef}
+    >
+      {sliderImageUrl.map((imageUrl, index) => {
+        return (
+          <div className="slider p-3 m-2 rounded-3 justify-content-center border border-secondary" key={index} >
+            <center className='mt-3'>
+                <img src={imageUrl.url} alt="movie" width={"150px"}className=''/>
+                <br></br>
+                <h4><b>{imageUrl.project} </b>Projects</h4>
+                <span>Completed In</span>
+                <br></br>
+                <b className='mt-2'>{imageUrl.name}</b>
+            </center>
+          </div>
+        );
+      })}
+    </Carousel>
+  </div>
+  );
+};
+
+export default Showcasing;
