41da3ab3a503b7f05d0cfd0721f1ecd787c1fd54
Shivamchhapriya <shivamchhapriya2003@gmail.com>
2023-11-04 18:25:52 +0530

    shivDev

---
+++ src/component/Achievements/index.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import './index.css'
+
+function Achievements() {
+    return (
+        <>
+            <section class="trending testi">
+<div class="">
+<div class="row">
+<div class="col-sm-6">
+<h3 class="ttl text-start">
+Celebrating Our Achievements In House Design &amp; Architecture
+</h3>
+</div>
+<div class="col-sm-6 text-end">
+<img alt="" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\g-reting.png?version=210" width="30%"/>
+</div>
+</div>
+<div class="row pt-4">
+<div class="col-sm-8">
+<div class="row">
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="Mr Ajay Naruka, Jaipur" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\jaipur_ofc.webp?version=210" height="250px;"/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>Mr Ajay Naruka, Jaipur</b>
+</p>
+</div>
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="Mr Abhimanyu Choudhary, Kota" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\kota_ofc.webp?version=210" height="250px;"/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>Mr Abhimanyu Choudhary, Kota</b>
+</p>
+</div>
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="Mr Shahbaz Alam, Patna" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\Patna_ofc.webp?version=210" height="250px;"/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>Mr Shahbaz Alam, Patna</b>
+</p>
+</div>
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="Mr Prasang Maheshwari, Dewas" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\dewas_ofc.png?version=210" height="250px;"/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>Mr Prasang Maheshwari, Dewas</b>
+</p>
+</div>
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="MMH Partner, Pune" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\OFC_DEMO.png?version=210" height="250px;"/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>MMH Partner, Pune</b>
+</p>
+</div>
+<div class="col-sm-4 mb-3">
+<div class="branch-img">
+<img alt="Discover More" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\jaipur_ofc.webp?version=210" height="250px;" style={{filter:'blir(8pc)'}}/>
+</div>
+<p class="text-center bg-dark text-white p-2">
+<b>Discover More</b>
+</p>
+</div>
+</div>
+</div>
+<div class="col-sm-4">
+<div class="row">
+<div class="col-sm-12 mb-3">
+<div class="sld-shad">
+<div class="row p-4">
+<div class="col-sm-4">
+<img alt="Red Achievers Awards 2021" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\awward-2021.png?version=210" width="100%"/>
+</div>
+<div class="col-sm-8 pt-4">
+<h5><b>Red Achievers Awards <font color="#3bad4b">2021</font></b></h5>
+<p>
+Awarded for exemplary performance and excellence in architecture and interior design.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="col-sm-12 mb-3">
+<div class="sld-shad">
+<div class="row p-4">
+<div class="col-sm-4 pt-2">
+<img alt="Best Startup 2022 by govt of India" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\awwor-2022.png?version=210" width="100%"/>
+</div>
+<div class="col-sm-8 pt-2">
+<h5><b>Best Startup <font color="#3bad4b"> 2022</font> by govt of India</b></h5>
+<p>
+awarded as top 25 startups in Indore by *government of India* Awarded by union minister Dr. Bhagwat Krishna Rao Ji, member of parliament Shri Shankar Ji Lalwani, Director MSME, and Director IIM Indore.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="col-sm-12 mb-3">
+<div class="sld-shad">
+<div class="row p-4">
+<div class="col-sm-4 text-center">
+<img alt="The Economic Times Real Estate Conclave &amp; Awards 2022" loading="lazy" src="https://www.makemyhouse.com/assets/themelibv3assets\images\web-images\awword-2022.png?version=210" height="100px;"/>
+</div>
+<div class="col-sm-8 pt-2">
+<h5><b>The Economic Times Real Estate Conclave &amp; Awards <font color="#3bad4b">2022
+</font> </b></h5>
+<p>
+e like to thank Economics Times Award for facilitating Make My House as the best Architectural firm of the year 2022.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+
+
+</div>
+</div>
+</section>
+        </>
+      );
+}
+
+export default Achievements;
---
+++ src/component/page/form.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import './index.css'
+
+function FormHeader() {
+    return (
+        <>
+            <div className="main_Form">
+            <form action="/action_page.php" className="d-flex">
+            <div class="form-group col-sm-2 pe-0 mb-0 has-error has-feedback input-Width">
+<input type="number" name="depth" class="form-control" id="length" aria-describedby="length" placeholder="Depth" />
+</div>
+<div class="form-group col-sm-2 pe-0 mb-0 ps-1 has-error has-feedback input-Width">
+<input type="number" name="width" class="form-control" id="width" aria-describedby="width" placeholder="Width"/>
+</div>
+<div class="form-group col-sm-2 pe-0 mb-0 ps-1 has-success input-Width">
+<select name="floors" id="floors" class="form-select" aria-invalid="false">
+<option value="1" selected="">Floor</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+</select>
+</div>
+<div class="form-group col-sm-2 pe-0 mb-0 ps-1 has-error has-feedback input-Width">
+<input name="build_area" type="text" class="form-control" id="builtUpArea" aria-describedby="builtUpArea" placeholder="Building Area"/>
+</div>
+<div class="form-group col-sm-2 pe-0 mb-0 ps-1 has-success input-Width">
+<select name="direction" id="direction" class="form-control" select2="" aria-invalid="false">
+<option selected="">Direction</option>
+<option value="NN" selected="">North</option>
+<option value="SS">South</option>
+<option value="SE">South East</option>
+<option value="NE">North East</option>
+<option value="EE">East</option>
+<option value="WW">West</option>
+<option value="SW">South West</option>
+<option value="NW">North West</option>
+</select>
+</div>
+
+<div class="form-group col-sm-2 ps-1 mb-0 input-Width" >
+<input type="hidden" id="product_id" name="product" value="75"/>
+<input type="hidden" name="couponcode" id="coupon_code" value="MMHPP15"/>
+<input type="hidden" name="campaignid" id="campaign_id" value="offerlane1"/>
+<button type="submit" id="btn_submit" value="Next" style={{background:'#3bad4b',border:'#3bad4b',color:'white'}} class="btn  w-100 mb-0">Calculate
+Price</button>
+</div>
+     </form>
+            </div>
+        </>
+      );
+}
+
+export default FormHeader;
