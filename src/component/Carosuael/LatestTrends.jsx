577c527ad6a8804b1b40aca91f296ce77b175417
Jay Ochhane <jay.o@theaquarious.com>
2023-11-10 14:11:50 +0530

    front Image change

--- src/component/Carosuael/LatestTrends.jsx
+++ src/component/Carosuael/LatestTrends.jsx
@@ -1,127 +1,127 @@
 import React, { useState, useRef } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import  './First.css'
+import './First.css'
 
-const LatestTrends= () => {
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 800 },
-          items: 4,
-          slidesToSlide: 1// optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 768 },
-          items: 4,
-          slidesToSlide: 4 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 767, min: 464 },
-          items: 2,
-          slidesToSlide: 2  // optional, default to 1.
-        }
-      };
-      const sliderImageUrl = [
-        //First image url
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1000sqft.webp?version=210",
-            size:"Area: 950sqft - 1100sqft"
-        },
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1150sqft.webp?version=210",
-            size:"Area: 1150sqft - 1300sqft"
-        },
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1350sqft.webp?version=210",
-            size:"Area: 1350sqft - 1500sqft"
-        },
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1750sqft.webp?version=210",
-            size:"Area: 1550sqft - 1800sqft"
-        },
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/2BHK.webp?version=210",
-            size:"Area: 1750sqft - 1900sqft"
-        },
-        {
-          url:
-            "	https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/6BHK.webp?version=210",
-            size:"Area: 1950sqft - 2100sqft"
-        },
-        {
-          url:
-            "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/6BHK.webp?version=210",
-            size:"Area: 2150sqft - 2300sqft"
-        },
-    
-      ];
+const LatestTrends = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 800 },
+      items: 4,
+      slidesToSlide: 1// optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 4,
+      slidesToSlide: 4 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 767, min: 464 },
+      items: 2,
+      slidesToSlide: 2  // optional, default to 1.
+    }
+  };
+  const sliderImageUrl = [
+    //First image url
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1000sqft.webp?version=210",
+      size: "Area: 950sqft - 1100sqft"
+    },
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1150sqft.webp?version=210",
+      size: "Area: 1150sqft - 1300sqft"
+    },
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1350sqft.webp?version=210",
+      size: "Area: 1350sqft - 1500sqft"
+    },
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/1750sqft.webp?version=210",
+      size: "Area: 1550sqft - 1800sqft"
+    },
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/2BHK.webp?version=210",
+      size: "Area: 1750sqft - 1900sqft"
+    },
+    {
+      url:
+        "	https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/6BHK.webp?version=210",
+      size: "Area: 1950sqft - 2100sqft"
+    },
+    {
+      url:
+        "https://www.makemyhouse.com/assets/themelibv2assets/app_assets/images/new/6BHK.webp?version=210",
+      size: "Area: 2150sqft - 2300sqft"
+    },
 
-      const carouselRef = useRef(null);
-      const CustomLeftButton = () => (
-        <button
-          className="custom-button left-button"
-          onClick={() => carouselRef.current.previous()}
-        >
-                  <img src="https://www.makemyhouse.com//assets/themelibv3assets/images/left-errow.png" alt="" />
+  ];
 
-        </button>
-      );
-    
-      const CustomRightButton = () => (
-        <button
-          className="custom-button right-button"
-          onClick={() => carouselRef.current.next()}
-        >
-         <img src="	https://www.makemyhouse.com//assets/themelibv3assets/images/right-errow.png" alt="" />
+  const carouselRef = useRef(null);
+  const CustomLeftButton = () => (
+    <button
+      className="custom-button left-button"
+      onClick={() => carouselRef.current.previous()}
+    >
+      <img src="https://www.makemyhouse.com//assets/themelibv3assets/images/left-errow.png" alt="" />
 
-        </button>
-      );
-      const CustomButtonGroup = (props) => {
-        return (
-          <div className="custom-button-group">
-          
-              <>
-                <CustomLeftButton {...props} />
-                <CustomRightButton {...props} />
-              </>
-       
-          </div>
-        );
-      };
+    </button>
+  );
+
+  const CustomRightButton = () => (
+    <button
+      className="custom-button right-button"
+      onClick={() => carouselRef.current.next()}
+    >
+      <img src="	https://www.makemyhouse.com//assets/themelibv3assets/images/right-errow.png" alt="" />
+
+    </button>
+  );
+  const CustomButtonGroup = (props) => {
+    return (
+      <div className="custom-button-group">
+
+        <>
+          <CustomLeftButton {...props} />
+          <CustomRightButton {...props} />
+        </>
+
+      </div>
+    );
+  };
   return (
 
-    <div className="px-3  tab-content" > 
-    <Carousel
-      responsive={responsive}
-      autoPlay={true}
-      arrows={false}
-      swipeable={true}
-      draggable={true}
-      showDots={false}
-      infinite={true}
-      pauseOnHover
-      partialVisible={false}
-      rtl={false}
-      autoPlaySpeed={2000} 
-      dotListClass="custom-dot-list-style"
+    <div className="px-3  tab-content" >
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        arrows={false}
+        swipeable={true}
+        draggable={true}
+        showDots={false}
+        infinite={true}
+        pauseOnHover
+        partialVisible={false}
+        rtl={false}
+        autoPlaySpeed={2000}
+        dotListClass="custom-dot-list-style"
         // customButtonGroup={<CustomButtonGroup />}
         ref={carouselRef}
-    >
-      {sliderImageUrl.map((imageUrl, index) => {
-        return (
-          <div className="slider p-1 rounded-5 justify-content-center" key={index}>
-            <img src={imageUrl.url} alt="movie" width={"100%"} height={"250px"} className='rounded-3 border border-secondary'/>
-            <center className='mt-3'><b>{imageUrl.size}</b></center>
-          </div>
-        );
-      })}
-    </Carousel>
-  </div>
+      >
+        {sliderImageUrl.map((imageUrl, index) => {
+          return (
+            <div className="slider p-1 rounded-5 justify-content-center" key={index}>
+              <img src={imageUrl.url} alt="movie" width={"100%"} height={"250px"} className='rounded-3 border border-secondary' />
+              <center className='mt-3'><b>{imageUrl.size}</b></center>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
 
--- src/component/page/frontPage.jsx
+++ src/component/page/frontPage.jsx
@@ -4,147 +4,147 @@ import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
-import {Box} from '@mui/material'
+import { Box } from '@mui/material'
 import FormHeader from "./form";
 import CardForm from "./headCard";
 // import { ThemeContext } from '@mui/styled-engine';
 
 
 function CustomTabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  CustomTabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
   };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+}
 function FrontPage() {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-    return ( 
-        <>
-            <div>
-               <div className="mainImage" >
-                    {/* <img src="/asset/frontPage.jpg" className="w-100" alt="" srcset="" /> */}
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <>
+      <div>
+        <div className="mainImage" >
+          {/* <img src="/asset/frontPage.jpg" className="w-100" alt="" srcset="" /> */}
 
-                   <div className="maincontainer" >
-                    <div className="frontHeader " >
-                        <h1  className="text-white fw-bold mt-5">अब घर बनेगा आसानी से</h1>    
-                    </div>
-                  <div className="row background_color">
-
-                    <div className="frist-grey  col-12 mt-5"  >
-                    <Box >
-                      <div className="background_tab_padding">
-      <Box sx={{ borderBottom: 1, borderColor: 'divider',background:'#DADADA',marginTop:'-40px'}} className='mx-lg-4 mx-md-2 mx-sm-1'>
-        <Tabs className="builder "  value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab  label={
-            <div>
-          <div >
-            <div className="tab_icon">
-            <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/2d.png?version=210" alt="Your Image" width="23" height="23" />
+          <div className="maincontainer" >
+            <div className="frontHeader " >
+              <h1 className="text-white fw-bold mt-5">अब घर बनेगा आसानी से</h1>
             </div>
-           
-  <span className="mt-1">2D Layout Plan</span> 
-          </div>
-          </div>
-        }   {...a11yProps(0)} />
-          <Tab  label={
-            <div>
-          <div >
-            <div className="tab_icon">
-            <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/3d.png?version=210" alt="Your Image" width="23" height="23" />
-            </div>
-          3D FRONT ELEVATION
-          </div>
-          </div>
-        }   {...a11yProps(1)} />
-           <Tab  label={
-          <div >
-            <div className="tab_icon">
-            <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/structual.png?version=210" alt="Your Image" width="23" height="23" />
-            </div>
-           STRUCTUAL DESIGN
-          </div>
-        }   {...a11yProps(2)} />
-         <Tab  label={
-          <div >
-            <div className="tab_icon">
-            <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/presentation.png?version=210" alt="Your Image" width="23" height="23" />
-            </div>
-             PRESENTATION PLAN           
-          </div>
-        }   {...a11yProps(3)} />
-         <Tab  label={
-          <div >
-            <div className="tab_icon">
-            <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/more-1.png?version=210" alt="Your Image" width="23" height="23" />
-            </div>
-           MORE
-          </div>
-        }   {...a11yProps(4)} />
-        </Tabs>
-      
-       
-      </Box>
-      </div>
-      <CustomTabPanel className='tab_body'  value={value} index={0}>
-       <FormHeader/>
-       
-      </CustomTabPanel>
-      <CustomTabPanel className='tab_body' value={value} index={1}>
-       <FormHeader/>
-        
-      </CustomTabPanel>
-      <CustomTabPanel className='tab_body' value={value} index={2}>
-      <FormHeader/>
+            <div className="row background_color">
 
-      </CustomTabPanel>
-      <CustomTabPanel className='tab_body' value={value} index={3}>
-      <FormHeader/>
+              <div className="frist-grey  col-12 mt-5"  >
+                <Box >
+                  <div className="background_tab_padding">
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider', background: '#DADADA', marginTop: '-40px' }} className='mx-lg-4 mx-md-2 mx-sm-1'>
+                      <Tabs className="builder " value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label={
+                          <div>
+                            <div >
+                              <div className="tab_icon">
+                                <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/2d.png?version=210" alt="Your Image" width="23" height="23" />
+                              </div>
 
-      </CustomTabPanel>
-      <CustomTabPanel className='tab_body_2' value={value} index={4}>
-      <CardForm/>
+                              <span className="mt-1">2D Layout Plan</span>
+                            </div>
+                          </div>
+                        }   {...a11yProps(0)} />
+                        <Tab label={
+                          <div>
+                            <div >
+                              <div className="tab_icon">
+                                <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/3d.png?version=210" alt="Your Image" width="23" height="23" />
+                              </div>
+                              3D FRONT ELEVATION
+                            </div>
+                          </div>
+                        }   {...a11yProps(1)} />
+                        <Tab label={
+                          <div >
+                            <div className="tab_icon">
+                              <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/structual.png?version=210" alt="Your Image" width="23" height="23" />
+                            </div>
+                            STRUCTUAL DESIGN
+                          </div>
+                        }   {...a11yProps(2)} />
+                        <Tab label={
+                          <div >
+                            <div className="tab_icon">
+                              <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/presentation.png?version=210" alt="Your Image" width="23" height="23" />
+                            </div>
+                            PRESENTATION PLAN
+                          </div>
+                        }   {...a11yProps(3)} />
+                        <Tab label={
+                          <div >
+                            <div className="tab_icon">
+                              <img src="https://www.makemyhouse.com/assets/themelibv3assets/images/web-images/more-1.png?version=210" alt="Your Image" width="23" height="23" />
+                            </div>
+                            MORE
+                          </div>
+                        }   {...a11yProps(4)} />
+                      </Tabs>
 
-      </CustomTabPanel>
-    </Box> 
-                    </div>
+
+                    </Box>
                   </div>
+                  <CustomTabPanel className='tab_body' value={value} index={0}>
+                    <FormHeader />
 
-                
-                    </div>
-               </div>
+                  </CustomTabPanel>
+                  <CustomTabPanel className='tab_body' value={value} index={1}>
+                    <FormHeader />
+
+                  </CustomTabPanel>
+                  <CustomTabPanel className='tab_body' value={value} index={2}>
+                    <FormHeader />
+
+                  </CustomTabPanel>
+                  <CustomTabPanel className='tab_body' value={value} index={3}>
+                    <FormHeader />
+
+                  </CustomTabPanel>
+                  <CustomTabPanel className='tab_body_2' value={value} index={4}>
+                    <CardForm />
+
+                  </CustomTabPanel>
+                </Box>
+              </div>
             </div>
-        </>
-     );
+
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FrontPage;
